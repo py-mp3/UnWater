70c44d87d301c91d2ca6bc62041383cfc1ca7ade
shubh-mp3 <shubh.mehta2019@gmail.com>
2025-01-30 18:01:06 +0000

    multiple images

--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,16 @@
-import { Text, View, TextInput, Image, Button} from "react-native";
+import { Text, View, TextInput, Image, Button } from "react-native";
 import { useState } from "react";
 
 export default function Index() {
-  const[text, setText] = useState("");
-  const[image, setImage] = useState("");
-    async function get_Image() {
-      const response = await fetch(`https://api.unsplash.com/search/photos?query=${text}&client_id=Mus_ZAgjrqDgnaom6Qi1nr00cKuexXpLCSMQeFyJ2Ik`)
-      const json = await response.json();
-      setImage(json.results[0].urls.regular)
-      }
+  const [text, setText] = useState("");
+  const [images, setImages] = useState([]);
+  async function get_Image() {
+    const response = await fetch(
+      `https://api.unsplash.com/search/photos?query=${text}&client_id=Mus_ZAgjrqDgnaom6Qi1nr00cKuexXpLCSMQeFyJ2Ik`
+    );
+    const json = await response.json();
+    setImages(json.results);
+  }
   return (
     <View
       style={{
@@ -19,9 +21,25 @@ export default function Index() {
       }}
     >
       <Text>Edit app/index.tsx to edit this screen.</Text>
-      <TextInput placeholder="Search here" value= {text} onChangeText={Text => setText(Text)}/>
-      <Image source={{uri: image}} style={{width: 200, height: 200}}/>
-      <Button title="Search" onPress={get_Image}/>
+      <TextInput
+        placeholder="Search here"
+        value={text}
+        onChangeText={(Text) => setText(Text)}
+      />
+      <Button title="Search" onPress={get_Image} />
+      <View style={{ flexDirection: "row", flexWrap: "wrap", padding: 10 }}>
+        {images.map((image) => {
+          return (
+            <View style={{ padding: 10 }}>
+              <Image
+                source={{ uri: image.urls.regular }}
+                style={{ width: 200, height: 200 }}
+              />
+            </View>
+          );
+        })}
+      </View>
+      
     </View>
   );
 }
